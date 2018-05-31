4bd57ed7e895cfe2b3feff3fc20ce8e2fad0272d
= <=>
2018-05-31 13:02:42 +0530

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  "use Strict";
+  let allNumbers = document.querySelectorAll('number');
+
+  allNumbers.forEach(function(elem) {
+    console.log(elem);
+  });
+
+
+}
